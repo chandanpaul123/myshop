270017965faaee323b55e8df512745c9af2287e9
Chandan Paul <paulchandan757@gmail.com>
2022-11-23 01:33:14 +0530

    myshop
    myshop

---
+++ bootstrap-5.1.3-dist/main.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+(function ($) {
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 0,
+    items: 1,
+    dots: true,
+    smartSpeed: 2000,
+    autoHeight: false,
+    autoplay: true,
+    responsiveClass: true,
+  });
+})(jQuery);
